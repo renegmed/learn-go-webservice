c7552d010be6632f78517bb06872b36faed3dd56
ren medal <rmedal@pop-os.localdomain>
2020-05-22 18:09:13 -0400

    03_handling_http: unmarshal and get product list in string json format

--- main.go
+++ main.go
@@ -1,23 +1,72 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/renegmed/inventoryservice/product"
 )
 
 type fooHandler struct {
 	Message string
 }
 
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+var productList []product.Product
+
+// func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte(f.Message))
+// }
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJson, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-type", "application/json")
+		w.Write(productsJson)
+	}
 }
 
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("bar called"))
+func init() {
+	productsJSON := `[
+{
+	"productId": 1,
+	"manufacturer": "Johns-Jenkins",
+	"sku": "p5z34vdS",
+	"upc": "939581000000",
+	"pricePerUnit": "497.45",
+	"quantityOnHand": 9703,
+	"productName": "sticky note"
+},
+{
+	"productId": 12,
+	"manufacturer": "Hessel, Schimmel and Feeney",
+	"sku": "i7v300kmx",
+	"upc": "740979000000",
+	"pricePerUnit": "282.29",
+	"quantityOnHand": 9217,
+	"productName": "leg warmers"
+},
+{
+	"productId": 3,
+	"manufacturer": "Swaniawski, Bartoletti and Bruen",
+	"sku": "q0L657ys7",
+	"upc": "11173000000",
+	"pricePerUnit": "436.26",
+	"quantityOnHand": 5905,
+	"productName": "lamp shade"
 }
-
+]`
+	err := json.Unmarshal([]byte(productsJSON), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 func main() {
-	http.Handle("/foo", &fooHandler{Message: "foo called"})
-	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/products", productsHandler)
 	http.ListenAndServe(":5000", nil)
 }
--- main_test.go
+++ main_test.go
@@ -5,59 +5,65 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 type httpResponse struct {
-	Body       string
-	StatusCode int
+	Body        string
+	StatusCode  int
+	ContentType string
 }
 
-func TestServerHTTP(t *testing.T) {
-	fooHandler := fooHandler{"Hello World!!!"}
-
-	request, err := newHandlerRequest("/foo")
-	if err != nil {
-		t.Fatalf("error while creating request, %v", err)
-	}
-	response := httptest.NewRecorder()
-
-	fooHandler.ServeHTTP(response, request)
-
-	httpResp, err := getHttpResponse(response)
-	if err != nil {
-		t.Fatalf("error from handler response, %v", err)
-	}
-
-	assertStatusCode(t, httpResp.StatusCode, 200)
-
-	assertResponseBody(t, httpResp.Body, fooHandler.Message)
-	if httpResp.Body != fooHandler.Message {
-		t.Errorf("Got response body  '%s' want '%s'", httpResp.Body, fooHandler.Message)
-	}
+func TestHandler(t *testing.T) {
+	wantJSON := `[
+{
+	"productId":1,
+	"manufacturer":"Johns-Jenkins",
+	"sku":"p5z34vdS",
+	"upc":"939581000000",
+	"pricePerUnit":"497.45",
+	"quantityOnHand":9703,
+	"productName":"sticky note"
+},
+{
+	"productId":12,
+	"manufacturer":"Hessel, Schimmel and Feeney",
+	"sku":"i7v300kmx",
+	"upc":"740979000000",
+	"pricePerUnit":"282.29",
+	"quantityOnHand":9217,
+	"productName":"leg warmers"
+},
+{
+	"productId":3,
+	"manufacturer":"Swaniawski, Bartoletti and Bruen",
+	"sku":"q0L657ys7",
+	"upc":"11173000000",
+	"pricePerUnit":"436.26",
+	"quantityOnHand":5905,
+	"productName":"lamp shade"
 }
+]`
+	t.Run("it should able to request for product list in json format", func(t *testing.T) {
+		request, err := newHandlerGetRequest("/products")
+		if err != nil {
+			t.Fatalf("error while creating request, %v", err)
+		}
+		response := httptest.NewRecorder()
 
-func TestGETHandler(t *testing.T) {
-	want := "bar called"
+		productsHandler(response, request)
 
-	request, err := newHandlerRequest("/bar")
-	if err != nil {
-		t.Fatalf("error while creating request, %v", err)
-	}
-	response := httptest.NewRecorder()
+		httpResp, err := getHttpResponse(response)
+		if err != nil {
+			t.Fatalf("error from handler response, %v", err)
+		}
 
-	barHandler(response, request)
+		assertStatusCode(t, httpResp.StatusCode, 200)
+		assertContentType(t, httpResp.ContentType, "application/json")
+		assertResponseJsonBody(t, httpResp.Body, wantJSON)
 
-	httpResp, err := getHttpResponse(response)
-	if err != nil {
-		t.Fatalf("error from handler response, %v", err)
-	}
-
-	assertStatusCode(t, httpResp.StatusCode, 200)
-
-	if httpResp.Body != want {
-		t.Errorf("Got response body  '%s' want '%s'", httpResp.Body, want)
-	}
+	})
 
 }
 
@@ -70,10 +76,11 @@ func getHttpResponse(response *httptest.ResponseRecorder) (httpResponse, error) 
 	}
 	httpResponse.Body = string(body)
 	httpResponse.StatusCode = resp.StatusCode
+	httpResponse.ContentType = resp.Header.Get("content-type")
 
 	return httpResponse, nil
 }
-func newHandlerRequest(url string) (*http.Request, error) {
+func newHandlerGetRequest(url string) (*http.Request, error) {
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprint(url), nil)
 	return req, err
 }
@@ -84,8 +91,32 @@ func assertStatusCode(t *testing.T, statusCode, want int) {
 	}
 }
 
+func assertContentType(t *testing.T, got, want string) {
+	if got != want {
+		t.Errorf("Response content type should be '%s' not '%s'", want, got)
+	}
+}
+
 func assertResponseBody(t *testing.T, got, want string) {
 	if got != want {
 		t.Errorf("Got response body  '%s' want '%s'", got, want)
 	}
+}
+
+func assertResponseJsonBody(t *testing.T, got, want string) {
+
+	// if !reflect.DeepEqual(got, want) { //got != want {
+	// 	t.Errorf("Got response body  '%s' want '%s'", got, want)
+	// }
+	got = cleanString(got)
+	want = cleanString(want)
+	if got != want {
+		t.Errorf("Got response body  \n'%s' want \n'%s'", got, want)
+	}
+}
+
+func cleanString(s string) string {
+	ret := strings.ReplaceAll(s, "\t", "")
+	ret = strings.ReplaceAll(ret, "\n", "")
+	return ret
 }
